ac869b7dba45e0c6ae7acb315bc22fce95aacc0a
Pankaj-Baranwal <pankajisadeveloper@gmail.com>
2017-02-03 23:47:32 +0530

    MyAccount Dialogs added.

--- app/src/main/java/com/fame/plumbum/lithicsin/fragments/MyAccount.java
+++ app/src/main/java/com/fame/plumbum/lithicsin/fragments/MyAccount.java
@@ -8,8 +8,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.fame.plumbum.lithicsin.R;
+import com.fame.plumbum.lithicsin.activities.MainActivity;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by pankaj on 12/1/17.
@@ -17,6 +33,9 @@ import com.fame.plumbum.lithicsin.R;
 
 public class MyAccount extends Fragment {
     View rootView;
+    RelativeLayout contact, official, misc;
+    LinearLayout dialog_contact, dialog_official, dialog_misc;
+
 
     @Nullable
     @Override
@@ -24,7 +43,183 @@ public class MyAccount extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_my_account, container, false);
 
         initCollapsingToolbar();
+        try {
+            Glide.with(getContext()).load(R.drawable.cover).into((ImageView) rootView.findViewById(R.id.backdrop));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        init();
+
     return rootView;
+    }
+
+    private void init() {
+        official = (RelativeLayout) rootView.findViewById(R.id.rl_official_details);
+        contact = (RelativeLayout) rootView.findViewById(R.id.rl_contact_info);
+        misc = (RelativeLayout) rootView.findViewById(R.id.rl_miscellaneous);
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout ll_dialog_official = (LinearLayout) rootView.findViewById(R.id.ll_dialog_contact);
+                ll_dialog_official.setVisibility(View.VISIBLE);
+                ((MainActivity)getActivity()).my_account.setVisibility(View.VISIBLE);
+                useDialogContact();
+            }
+        });
+        official.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout ll_dialog_official = (LinearLayout) rootView.findViewById(R.id.ll_dialog_official);
+                ll_dialog_official.setVisibility(View.VISIBLE);
+                ((MainActivity)getActivity()).my_account.setVisibility(View.VISIBLE);
+                useDialogOfficial();
+            }
+        });
+        misc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout ll_dialog_official = (LinearLayout) rootView.findViewById(R.id.ll_dialog_misc);
+                ll_dialog_official.setVisibility(View.VISIBLE);
+                ((MainActivity)getActivity()).my_account.setVisibility(View.VISIBLE);
+                useDialogMisc();
+            }
+        });
+    }
+
+    private void useDialogOfficial() {
+        MaterialEditText tin = (MaterialEditText) rootView.findViewById(R.id.tin);
+        MaterialEditText cst = (MaterialEditText) rootView.findViewById(R.id.cst);
+        Button permanent = (Button) rootView.findViewById(R.id.permanent_address);
+        final Button pickup = (Button) rootView.findViewById(R.id.pickup_address);
+        Button categories = (Button) rootView.findViewById(R.id.product_categories);
+        Button update = (Button) rootView.findViewById(R.id.update);
+        Button cancel = (Button) rootView.findViewById(R.id.cancel);
+        CheckBox pickup_checkbox = (CheckBox) rootView.findViewById(R.id.checkbox_pickup);
+        TextView heading = (TextView) rootView.findViewById(R.id.heading_my_account_dialog);
+        Spinner state_of_operation = (Spinner) rootView.findViewById(R.id.state_of_operation);
+        heading.setText("Official Details");
+        pickup_checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    pickup.setClickable(false);
+                    pickup.setBackgroundColor(0x886db096);
+                }else{
+                    pickup.setClickable(true);
+                    pickup.setBackgroundColor(0xff6db096);
+                }
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).my_account.setVisibility(View.GONE);
+            }
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Add data in database
+                ((MainActivity)getActivity()).my_account.setVisibility(View.GONE);
+            }
+        });
+        permanent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        pickup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        categories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        List<String> states = Arrays.asList("Choose your state", "Andaman and Nicobar Islands", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chandigarh", "Chhattisgarh", "Dadra and Nagar Haveli", "Daman and Diu", "Delhi", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Lakshadweep", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Puducherry", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, states);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        state_of_operation.setAdapter(adapter);
+        state_of_operation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private void useDialogContact() {
+        MaterialEditText name_of_organization= (MaterialEditText) rootView.findViewById(R.id.name_organization);
+        MaterialEditText email_id = (MaterialEditText) rootView.findViewById(R.id.email_id);
+        MaterialEditText contact_name = (MaterialEditText) rootView.findViewById(R.id.contact_name);
+        MaterialEditText mobile_no = (MaterialEditText) rootView.findViewById(R.id.mobile_no);
+        Button update = (Button) rootView.findViewById(R.id.update);
+        Button cancel = (Button) rootView.findViewById(R.id.cancel);
+        TextView heading = (TextView) rootView.findViewById(R.id.heading_my_account_dialog);
+        Spinner state_of_operation = (Spinner) rootView.findViewById(R.id.state_of_operation);
+        heading.setText("Contact Details");
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).my_account.setVisibility(View.GONE);
+            }
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Add data in database
+                ((MainActivity)getActivity()).my_account.setVisibility(View.GONE);
+            }
+        });
+        List<String> states = Arrays.asList("Choose your state", "Andaman and Nicobar Islands", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chandigarh", "Chhattisgarh", "Dadra and Nagar Haveli", "Daman and Diu", "Delhi", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Lakshadweep", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Puducherry", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, states);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        state_of_operation.setAdapter(adapter);
+        state_of_operation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private void useDialogMisc() {
+        Button update = (Button) rootView.findViewById(R.id.update);
+        Button cancel = (Button) rootView.findViewById(R.id.cancel);
+        TextView heading = (TextView) rootView.findViewById(R.id.heading_my_account_dialog);
+        heading.setText("Miscellaneous Questions");
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).my_account.setVisibility(View.GONE);
+            }
+        });
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Add data in database
+                ((MainActivity)getActivity()).my_account.setVisibility(View.GONE);
+            }
+        });
     }
 
     /**
