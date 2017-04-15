bc6f5187f3e5edd9930f75f9f6f10ba91a247577
Pankaj-Baranwal <pankajisadeveloper@gmail.com>
2017-04-16 03:35:08 +0530

    res images and ChooseImage interface

---
+++ app/src/main/java/com/fame/plumbum/lithicsin/interfaces/Choose_image.java
@@ -0,0 +1,12 @@
+package com.fame.plumbum.lithicsin.interfaces;
+
+/**
+ * Project Name: 	<Visual Perception For The Visually Impaired>
+ * Author List: 		Pankaj Baranwal
+ * Filename: 		<>
+ * Functions: 		<>
+ * Global Variables:	<>
+ */
+public interface Choose_image {
+    void load_image_picker();
+}
