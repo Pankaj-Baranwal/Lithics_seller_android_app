0218ebc1a808b0fdd9790721908cb7932b25d6c0
Pankaj-Baranwal <pankajisadeveloper@gmail.com>
2017-04-16 03:33:35 +0530

    On Registration, wait for seller relational linkage

---
+++ app/src/main/java/com/fame/plumbum/lithicsin/activities/PleaseWait.java
@@ -0,0 +1,23 @@
+package com.fame.plumbum.lithicsin.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fame.plumbum.lithicsin.R;
+
+/**
+ * Project Name: 	<Visual Perception For The Visually Impaired>
+ * Author List: 		Pankaj Baranwal
+ * Filename: 		<>
+ * Functions: 		<>
+ * Global Variables:	<>
+ */
+public class PleaseWait extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait);
+    }
+}
